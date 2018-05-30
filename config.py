1d1d3a486e9c91ac6fa858b0d372cfd443940d7c
jainsaket1994 <jainsaket.1994@gmail.com>
2018-05-30 15:03:04 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+config = {
+
+    'ec2_region_name': 'us-west-2',
+    'ec2_region_endpoint': 'ec2.us-west-2.amazonaws.com',
+    'aws_access_key' : '**************************',
+    'aws_secret_key' : '*****************************************',
+    'owner' : '******************************',
+    'prod_vol_id' : '****************',
+    'prod_days_old' : '30',
+    'tag_name': 'Name',
+    'tag_value': 'prod_mongo_snapshot-',
+}
